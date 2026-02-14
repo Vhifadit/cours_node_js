c0bbf555f84c5eb4f33bf78d6280c8bb1b56d6c9
vhifadit <daagbohounondekavhifadit@gmail.com>
2026-02-14 12:11:23 +0100

    mise en place du module d'authentification

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
+import "dotenv/config";
 import express from 'express'
 import argon2 from 'argon2'
 import { Utilisateur } from './user.service.js'
+import { validateData } from "./lib/validate.js";
+import { loginSchema } from "./schemas/user.schema.js";
+import { signToken } from "./lib/jwt.js";
 
 const app = express()
 app.use(express.json())
@@ -13,17 +17,20 @@ app.post('/auth/register', async (req,res,next) =>{
   res.json(await user.create(req.body));
 
 })
-app.post('auth/login',(req,res,next)=>{
+app.post('/auth/login',async (req,res,next)=>{
+ const data =  validateData(loginSchema, req.body)// validation 
 
+ const user = new Utilisateur()
+ const utilisateurloge = await user.login(data.email,data.password)
+ const token = await signToken({ userId: utilisateurloge.id })
+ res.json({
+      success: true,
+      user: utilisateurloge,
+      token,
+    });
 })
 
 
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
--- backend/user.service.js
+++ backend/user.service.js
@@ -1,16 +1,29 @@
 import argon2 from "argon2";
 import db from "./database.js";
+import { hashPassword } from "./lib/password.js";
 
 export class Utilisateur { 
     
 
 async create (data) { 
-    const hash = await argon2.hash(data.password);
-    console.log(`Nom: ${this.nom}, Email: ${this.email}`);
+    console.log(`Nom: ${data.nom}, Email: ${data.email}`);
+    const hash = await hashPassword(data.password);
     db.prepare("INSERT INTO users (nom, email, mot_de_passe) VALUES (?,?,?);")
-    .run(data.name,data.email,hash)
+    .run(data.nom,data.email,hash)
     return {...data, password: hash}
 
  }
+   async login(email, password) {
+
+     const user = db.prepare("SELECT * FROM users WHERE email = ?;").get(email);
+
+     if (!user || !(await argon2.verify(user.mot_de_passe, password))) {
+
+         throw new Error("Identifiants invalides"); 
+
+        } 
+    console.log(`Connexion réussie pour: ${user.nom}, Email: ${user.email}`); 
+        return user; 
+    }
 
 } 
